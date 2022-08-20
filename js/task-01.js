a84783d90f62c7198d4b9e0be0a47220fe594a16
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-08-21 00:00:48 +0300

    Update task-01.js
    resolve task 1

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+
+const catEl = document.querySelectorAll('.item');
+
+const nameEl = document.querySelectorAll('.item > h2');
+
+const numberOfItemsEl = document.querySelectorAll('.item ul');
+
+console.log(`Number of categories: ${catEl.length}`);
+console.log(`Category: ${nameEl[0].textContent}`);
+console.log(`Elements: ${numberOfItemsEl[0].childElementCount}`);
+console.log(`Category: ${nameEl[1].textContent}`);
+console.log(`Elements: ${numberOfItemsEl[1].childElementCount}`);
+console.log(`Category: ${nameEl[2].textContent}`);
+console.log(`Elements: ${numberOfItemsEl[2].childElementCount}`);
+
+
+
+
+
+
+
+
+
