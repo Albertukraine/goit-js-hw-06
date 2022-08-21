15135d1e58ec35fbc3c77fbe920ee7ceb577e3f9
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-08-22 01:45:03 +0300

    add anonymous if input is clear

--- js/task-05.js
+++ js/task-05.js
@@ -4,6 +4,9 @@ const textOutput = document.getElementById("name-output");
 textInput.addEventListener("input", (event) => {
 //   let name = event.currentTarget.value;
   textOutput.textContent = event.currentTarget.value.trim();
+  if (event.currentTarget.value === "") {
+    textOutput.textContent = 'Anonymous'
+  };
 });
 
 console.log(textInput);
