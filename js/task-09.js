d3bb0f90937fa39c50c2700cf43c44e8e5a2bc79
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-08-22 19:14:45 +0300

    resolve task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const colorBtn = bodyEl.querySelector('.change-color');
+
+
+colorBtn.addEventListener('click', () => {
+  let currentColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = currentColor;
+});
+
