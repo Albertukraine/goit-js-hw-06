47a1aba9612ef1d00ccbab140de4bcc29262e437
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-08-22 23:03:59 +0300

    resolve task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const formEl = document.querySelector('.login-form');
+console.log(formEl);
+
+formEl.addEventListener("submit", (event) => {
+event.preventDefault();
+const { elements: { email, password }} = event.currentTarget;
+
+console.log(email.value, password.value);
+console.log(email.value.length, password.value.length);
+
+if (email.value.length === 0 && password.value.length === 0) {
+   alert('Хазяїне, введіть щось в ті поля!')
+};
+if (email.value.length !== 0 && password.value.length !== 0) {
+   const userData = {};
+   userData.email = email.value;
+   userData.password = password.value;
+   console.log(userData);
+   formEl.reset();
+}
+
+});
+
+
+
+
+
+
 
