46178607ed98722b11c0112d857e21fb22dd0a0d
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-08-23 17:05:18 +0300

    resolve task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const valueEl = document.querySelector("input");
+const createButtonEl = document.querySelector("[data-create]");
+const destroyButtonEl = document.querySelector("[data-destroy]");
+const boxesEl = document.getElementById("boxes");
+
+console.log(valueEl);
+
+console.log(createButtonEl);
+console.log(destroyButtonEl);
+console.log(boxesEl);
+
+createButtonEl.addEventListener("click", () => {
+  let currentValue = valueEl.value;
+  console.log("текущее значение", +currentValue);
+  let size = 30;
+  let count = +currentValue;
+  for (let i = 0; i < count; i += 1) {
+    const boxJS = document.createElement("div");
+
+    size += 10;
+    boxJS.style.width = `${size}px`;
+    boxJS.style.height = `${size}px`;
+    boxJS.style.backgroundColor = getRandomHexColor();
+    boxesEl.append(boxJS);
+  }
+});
+
+destroyButtonEl.addEventListener("click", () => {
+  boxesEl.remove("div");
+});
